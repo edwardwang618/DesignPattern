ec5b5fa644222fee532b4240bb90c8168d302fbd
edwardwang618 <edward.wang618@gmail.com>
2021-01-27 00:57:36 -0600

    Single Responsibility and Interface Segregation

--- src/com/atguigu/principle/segregation/improve/Segregation1.java
+++ src/com/atguigu/principle/segregation/improve/Segregation1.java
@@ -1,24 +1,36 @@
-package com.atguigu.principle.segregation;
+package com.atguigu.principle.segregation.improve;
 
 public class Segregation1 {
     public static void main(String[] args) {
+        A a = new A();
+        a.depend1(new B());
+        a.depend2(new B());
+        a.depend3(new B());
     
+        C c = new C();
+        c.depend1(new D());
+        c.depend4(new D());
+        c.depend5(new D());
     }
 }
 
 interface Interface1 {
     void operation1();
-    
+}
+
+interface Interface2 {
     void operation2();
     
     void operation3();
-    
+}
+
+interface Interface3 {
     void operation4();
     
     void operation5();
 }
 
-class B implements Interface1 {
+class B implements Interface1, Interface2 {
     
     @Override
     public void operation1() {
@@ -34,33 +46,13 @@ class B implements Interface1 {
     public void operation3() {
         System.out.println("B中实现了operation3");
     }
-    
-    @Override
-    public void operation4() {
-        System.out.println("B中实现了operation4");
-    }
-    
-    @Override
-    public void operation5() {
-        System.out.println("B中实现了operation5");
-    }
 }
 
-class D implements Interface1 {
+class D implements Interface1, Interface3 {
     
     @Override
     public void operation1() {
         System.out.println("D中实现了operation1");
-    }
-    
-    @Override
-    public void operation2() {
-        System.out.println("D中实现了operation2");
-    }
-    
-    @Override
-    public void operation3() {
-        System.out.println("D中实现了operation3");
     }
     
     @Override
@@ -79,11 +71,11 @@ class A {
         i.operation1();
     }
     
-    public void depend2(Interface1 i) {
+    public void depend2(Interface2 i) {
         i.operation2();
     }
     
-    public void depend3(Interface1 i) {
+    public void depend3(Interface2 i) {
         i.operation3();
     }
 }
@@ -93,11 +85,11 @@ class C {
         i.operation1();
     }
     
-    public void depend4(Interface1 i) {
+    public void depend4(Interface3 i) {
         i.operation4();
     }
     
-    public void depend5(Interface1 i) {
+    public void depend5(Interface3 i) {
         i.operation5();
     }
 }
